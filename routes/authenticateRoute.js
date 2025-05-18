cec8c318d93b01b400d4f4078936af01ce7b04bb
AliArif5781 <aliairf92@gmail.com>
2025-05-19 02:23:06 +0500

    authenticateRoute

--- routes/authenticateRoute.js
+++ routes/authenticateRoute.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { signup } from "../controllers/authenticate.controllers.js";
+import { login, signup } from "../controllers/authenticate.controllers.js";
 
 export const authenticateRoute = express.Router();
 
-authenticateRoute.post("/", signup);
+authenticateRoute.post("/signup", signup);
+authenticateRoute.post("/login", login);
