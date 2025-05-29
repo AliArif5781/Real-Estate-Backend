d41d90ec554a06e90a44375ca4c44c9880718cce
AliArif5781 <aliairf92@gmail.com>
2025-05-29 14:11:56 +0500

    Add user.controllers.js

---
+++ controllers/user.controllers.js
@@ -0,0 +1,24 @@
+import { userModel } from "../models/user.model.js";
+
+export const getUserData = async (req, res) => {
+  try {
+    const user = await userModel.findById(req.userId);
+    console.log(user);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found " });
+    }
+    res.json({
+      success: true,
+      userData: {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        isAccountVerified: user.isAccountVerified,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server error." });
+  }
+};
