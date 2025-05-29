83a957ec9acefcd8ac5ed628c89d4d85734c0612
AliArif5781 <aliairf92@gmail.com>
2025-05-29 14:11:55 +0500

    Update authenticateRoute.js

--- routes/authenticateRoute.js
+++ routes/authenticateRoute.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { login, signup } from "../controllers/authenticate.controllers.js";
+import { userAuth } from "../middlewares/user.auth.js";
 
 export const authenticateRoute = express.Router();
 
 authenticateRoute.post("/signup", signup);
-authenticateRoute.post("/login", login);
+authenticateRoute.post("/login", userAuth, login);
