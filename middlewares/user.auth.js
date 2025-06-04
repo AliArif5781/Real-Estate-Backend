496c6aa84f3e0fc7a3b7aeb01c3653a1daf024b0
AliArif5781 <aliairf92@gmail.com>
2025-06-04 16:56:12 +0500

    Update user.auth.js

--- middlewares/user.auth.js
+++ middlewares/user.auth.js
@@ -10,7 +10,7 @@ export const userAuth = async (req, res, next) => {
   }
   try {
     const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
-    console.log(tokenDecode);
+    // console.log(tokenDecode);
 
     if (tokenDecode.id) {
       req.userId = tokenDecode.id;
