10c2651ab7de2d6f1ba3736b4d4e687c05ad52c3
AliArif5781 <aliairf92@gmail.com>
2025-06-15 23:49:22 +0500

    Update postRoute.js

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -1,7 +1,14 @@
 import express from "express";
-import { getAllPost, post } from "../controllers/post.controllers.js";
+import {
+  getAllPost,
+  getPostById,
+  getSearchPost,
+  post,
+} from "../controllers/post.controllers.js";
 
 export const postRoute = express.Router();
 
 postRoute.post("/post", post);
 postRoute.get("/getPost", getAllPost);
+postRoute.get("/searchPost", getSearchPost);
+postRoute.get("/:id", getPostById);
