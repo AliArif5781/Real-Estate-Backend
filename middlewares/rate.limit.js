b99f8388cb26cc09d602ce58c204035f3504a498
AliArif5781 <aliairf92@gmail.com>
2025-07-12 01:55:00 +0500

    Update rate.limit.js

--- middlewares/rate.limit.js
+++ middlewares/rate.limit.js
@@ -1,17 +1,13 @@
 import rateLimit from "express-rate-limit";
 
 export const postLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 30 * 1000, // 1 minute (60 seconds)
   max: 1, // Limit each user to 1 post per windowMs
   message: {
     success: false,
-    message: "You can only create one property post every 15 minutes",
+    message: "You can only create one property post every minute",
   },
-  keyGenerator: (req) => {
-    // Assuming you have user authentication
-    // console.log(req.userId, "req");
-    return req.userId.toString(); // Convert to string if using Mongoose
-  },
-  standardHeaders: true, // Return rate limit info in headers
-  legacyHeaders: false, // Disable deprecated headers
+  keyGenerator: (req) => req.userId.toString(),
+  standardHeaders: true,
+  legacyHeaders: false,
 });
