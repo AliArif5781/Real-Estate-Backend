9245ec988ccbd03d7d2d71aa2d2a2b9f1c2df498
AliArif5781 <aliairf92@gmail.com>
2025-07-12 01:11:22 +0500

    Update postRoute.js

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -5,10 +5,16 @@ import {
   getSearchPost,
   post,
 } from "../controllers/post.controllers.js";
+import rateLimit from "express-rate-limit";
+import { postLimiter } from "../middlewares/rate.limit.js";
+import { userAuth } from "../middlewares/user.auth.js";
 
 export const postRoute = express.Router();
 
-postRoute.post("/post", post);
+postRoute.post("/post"
+  , userAuth,
+  //  postLimiter
+  , post);
 postRoute.get("/getPost", getAllPost);
 postRoute.get("/searchPost", getSearchPost);
 postRoute.get("/:id", getPostById);
