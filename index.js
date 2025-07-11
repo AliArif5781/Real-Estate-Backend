9ff4055b3bae32ff9c39c02bd8c49a81b325a22c
AliArif5781 <aliairf92@gmail.com>
2025-07-12 01:56:33 +0500

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "real-estate-frontend-omega-mocha.vercel.app",
+      "https://real-estate-frontend-omega-mocha.vercel.app",
     ],
     credentials: true,
   })
