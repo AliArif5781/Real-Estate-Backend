0223c3cf2b9baf76792bc09ff7b4622427b6fbb4
AliArif5781 <aliairf92@gmail.com>
2025-07-12 12:41:51 +0500

    Update authenticate.controllers.js

--- controllers/authenticate.controllers.js
+++ controllers/authenticate.controllers.js
@@ -6,11 +6,11 @@ export const signup = async (req, res) => {
   try {
     const { firstName, lastName, email, password } = req.body;
 
-    if (!firstName || !lastName || !email || !password) {
-      return res
-        .status(400)
-        .json({ success: false, message: "All fields are required." });
-    }
+    // if (!firstName || !lastName || !email || !password) {
+    //   return res
+    //     .status(400)
+    //     .json({ success: false, message: "All fields are required." });
+    // }
 
     const existingUser = await userModel.findOne({ email });
     if (existingUser) {
@@ -35,7 +35,7 @@ export const signup = async (req, res) => {
       // when we run this project on live server then it will run on https then it will be true, while running in local environment/development environment it will run on http it mean not secureit will be false, then make it true or false make env.
       secure: process.env.NODE_ENV === "production", // so secure will be false for development env it will true in production env.
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7d in milli-second
+      // maxAge: 7 * 24 * 60 * 60 * 1000, // 7d in milli-second
     });
     return res
       .status(200)
@@ -75,8 +75,8 @@ export const login = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-      path: "/",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
+      // path: "/",
+      // maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res
       .status(200)
