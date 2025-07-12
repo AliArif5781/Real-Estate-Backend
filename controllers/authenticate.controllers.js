3458437a85f2f560c8bea7e6f78c47d2c81c84f6
AliArif5781 <aliairf92@gmail.com>
2025-07-12 14:19:53 +0500

    Update authenticate.controllers.js

--- controllers/authenticate.controllers.js
+++ controllers/authenticate.controllers.js
@@ -6,11 +6,11 @@ export const signup = async (req, res) => {
   try {
     const { firstName, lastName, email, password } = req.body;
 
-    // if (!firstName || !lastName || !email || !password) {
-    //   return res
-    //     .status(400)
-    //     .json({ success: false, message: "All fields are required." });
-    // }
+    if (!firstName || !lastName || !email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required." });
+    }
 
     const existingUser = await userModel.findOne({ email });
     if (existingUser) {
@@ -32,10 +32,10 @@ export const signup = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true,
-      // when we run this project on live server then it will run on https then it will be true, while running in local environment/development environment it will run on http it mean not secureit will be false, then make it true or false make env.
-      secure: process.env.NODE_ENV === "production", // so secure will be false for development env it will true in production env.
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-      // maxAge: 7 * 24 * 60 * 60 * 1000, // 7d in milli-second
+      // // when we run this project on live server then it will run on https then it will be true, while running in local environment/development environment it will run on http it mean not secureit will be false, then make it true or false make env.
+      // secure: process.env.NODE_ENV === "production", // so secure will be false for development env it will true in production env.
+      // sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      // // maxAge: 7 * 24 * 60 * 60 * 1000, // 7d in milli-second
     });
     return res
       .status(200)
@@ -73,8 +73,8 @@ export const login = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      // secure: process.env.NODE_ENV === "production",
+      // sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       // path: "/",
       // maxAge: 7 * 24 * 60 * 60 * 1000,
     });
