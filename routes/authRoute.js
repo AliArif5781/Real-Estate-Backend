cf5e189a911be0c663bda277f60ed05bbc338ecb
AliArif5781 <aliairf92@gmail.com>
2025-07-15 22:18:33 +0500

    Update authRoute.js

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,3 +5,6 @@ import { getUserData } from "../controllers/user.controllers.js";
 export const userRoutes = expres.Router();
 
 userRoutes.get("/data", userAuth, getUserData);
+userRoutes.get("/protected", userAuth, (req, res) => {
+  res.json({ message: "This is a protected route", user: req.userId });
+});
