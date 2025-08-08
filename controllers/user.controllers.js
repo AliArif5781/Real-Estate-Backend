ed89b677985c5af9b0bb5f902bb7497a0ce3cb58
AliArif5781 <aliairf92@gmail.com>
2025-08-09 01:07:25 +0500

    Update user.controllers.js

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -39,3 +39,32 @@ export const getAllUserDetail = async (req, res) => {
     res.status(500).json({ success: false, message: error.message });
   }
 };
+
+export const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await userModel.findById(id);
+
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    await userModel.findByIdAndDelete(id);
+
+    // console.log("Deleted user ID:", id);
+    res.status(200).json({
+      success: true,
+      message: "User deleted successfully",
+      deletedUserId: id,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Server error while deleting user",
+      error: error.message,
+    });
+  }
+};
