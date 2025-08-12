038fe6ece2627281d6a5b3a77e27f8f6e4f84c75
AliArif5781 <aliairf92@gmail.com>
2025-08-12 09:04:07 +0500

    Update index.js

--- index.js
+++ index.js
@@ -17,10 +17,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://real-estate-frontend-omega-mocha.vercel.app",
-    ],
+    origin: ["http://localhost:5173", "real-estate-frontend-weld.vercel.app"],
     credentials: true,
   })
 );
