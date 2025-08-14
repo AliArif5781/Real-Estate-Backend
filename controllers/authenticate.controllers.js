07090e93af3ce1d097d1458fa672a1ff2ad99a53
AliArif5781 <aliairf92@gmail.com>
2025-08-14 11:41:14 +0500

    Update authenticate.controllers.js

--- controllers/authenticate.controllers.js
+++ controllers/authenticate.controllers.js
@@ -29,10 +29,15 @@ export const signup = async (req, res) => {
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     res.cookie("token", token, {
+      // httpOnly: true,
+      // // // secure: process.env.NODE_ENV === "production", // Enable in production
+      // // // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // Adjust for production
+      // maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days expiry example
+
       httpOnly: true,
-      // secure: process.env.NODE_ENV === "production", // Enable in production
-      // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // Adjust for production
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days expiry example
+      secure: true, // Enable in production (Vercel uses HTTPS)
+      sameSite: "none", // Required for cross-origin cookies
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
     return res
       .status(200)
@@ -70,10 +75,14 @@ export const login = async (req, res) => {
       process.env.JWT_SECRET
     );
     res.cookie("token", token, {
+      // httpOnly: true,
+      // // // secure: process.env.NODE_ENV === "production", // Enable in production
+      // // // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // Adjust for production
+      // maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days expiry example
       httpOnly: true,
-      // secure: process.env.NODE_ENV === "production", // Enable in production
-      // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // Adjust for production
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days expiry example
+      secure: true, // Enable in production
+      sameSite: "none", // Required for cross-origin cookies
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
     return res.status(200).json({
       success: true,
@@ -104,5 +113,3 @@ export const logoutUser = async (req, res) => {
     });
   }
 };
-
-
